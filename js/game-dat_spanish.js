975fd9e44accab31a359947131ca417cee947b8e
kendra.jade <kindrai.kendra@gmail.com>
2026-09-18 13:19:40 -0500

    generateWord added, developing synonymCheck

---
+++ js/game-dat_spanish.js
@@ -0,0 +1,101 @@
+/** 
+ * @author kkindrai
+ * @implements 
+ *  https://freedictionaryapi.com/
+ *  https://random-word-api.herokuapp.com/home
+*/
+
+/**
+ * ────୨୧────────୨୧────────୨୧────
+ * Controller - START
+*/
+
+function controller() {
+    let word;
+    let hasSynonyms = false;
+
+    word = generateWord();
+    synonymCheck(word);
+
+    // while (!hasSynonyms) {
+    //     word = generateWord();
+    //     results = synonymCheck(word);
+    // }
+    
+    
+}
+
+/**
+ * Controller - END
+ * ────୨୧────────୨୧────────୨୧────
+*/
+
+/**
+ * ────୨୧────────୨୧────────୨୧────
+ * generateWord - START
+ * 
+ * Pulls a word from the random-word-api
+*/
+
+function generateWord() {
+    // Declare local variables
+    let url = 'https://random-word-api.herokuapp.com/word?lang=es';
+    let word;
+
+    // Obtain word from API
+    word = JSON.parse(requestData(url))[0];
+
+    console.log(`[generateWord] word = ${word}`);
+
+    return word;
+}
+
+/**
+ * generateWord - END
+ * ────୨୧────────୨୧────────୨୧────
+*/
+
+/**
+ * ────୨୧────────୨୧────────୨୧────
+ * synonymCheck - START
+*/
+
+function synonymCheck(word) {
+    // Declare local variables
+    // DEV
+    word = 'chingar';
+    let url = 'https://freedictionaryapi.com/api/v1/entries/es/' + word;
+    let results;
+    let synDataLocation01;
+    let synDataLocation02;
+    let synDataLocation03;
+
+    let synonymLib = [];
+
+    results = requestData(url);
+
+    console.log(`[synonymCheck] url = ${url}`);
+
+    console.log(`[synonymCheck] results = ${results}`);
+
+    // So... there's a lot of weird places it saves synonyms for some reason, and I hate it.
+    
+    // Synonym Data Locations
+    synDataLocation01 = results.entries;
+
+    if (synDataLocation01) {
+        synDataLocation01.forEach(entry => {
+            console.log(`[synonymCheck] entry.synonyms = ${entry.synonyms}`);
+        });
+    }
+
+    return results;
+}
+
+/**
+ * synonymCheck - END
+ * ────୨୧────────୨୧────────୨୧────
+*/
+
+
+window.onload = controller;
---
+++ js/utils.js
@@ -0,0 +1,13 @@
+ /** ────୨୧────────୨୧────
+ * requestData Function - START
+*/
+function requestData(requestURL) {
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",requestURL,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+/**
+ * Request Function - END
+ * ────୨୧────────୨୧────
+*/
